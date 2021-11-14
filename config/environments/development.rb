d506c98afa8354f457d49027d0480a18498b2fd1
t-techa2020 <t.techa2020@gmail.com>
2021-11-14 17:41:07 +0900

    5-3-2 メール送信の動作確認

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.action_mailer.delivery_method = :file
+  # config.aaction_mailer.file_settings = { location: Rails.root.join('log/mails') } # ファイル保存パスの指定
+  # 指定しないとデフォルトでは tmp/mails 以下に保存される
 end
